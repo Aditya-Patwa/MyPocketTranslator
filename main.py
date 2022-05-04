cb47aa6abf3d9985a684a3cb11ce039773caed21
Aditya Patwa <adityapatwa.tech@gmail.com>
2022-05-04 09:41:40 +0000

    Inital Commit

---
+++ main.py
@@ -0,0 +1,33 @@
+import streamlit as st
+from PyDictionary import PyDictionary
+
+dictionary = PyDictionary()
+
+st.title("MyPocketTranslator")
+# languages = []
+# fromLang = st.selectbox(
+#      'From',
+#      ('en', 'fr', 'es', 'hi'))
+
+# toLang = st.selectbox(
+#      'To',
+#      ('en', 'fr', 'es', 'hi'))
+
+textInput = st.text_input('Word to Translate')
+translation = dictionary.meaning(textInput)
+
+st.header("Meanings")
+for key, value in translation.items():
+     words = ", ".join(value)
+     st.markdown(f"""
+     ##### {key}
+     {words}
+     """)
+
+st.header("Synonyms")
+synonym = dictionary.synonym(textInput)
+st.markdown(f"{synonym}")
+
+st.header("Antonyms")
+antonym = dictionary.antonym(textInput)
+st.markdown(f"{antonym}")
